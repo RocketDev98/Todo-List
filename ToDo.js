28d77b7d48bf96264b7010ca1d49fc8a0c7a2925
RocketDev98 <ben_ripley@hotmail.com>
2022-07-22 09:18:14 -0600

    fully functioning todo

--- ToDo.js
+++ ToDo.js
@@ -9,12 +9,19 @@ window.setTimeout(function () {
         if (entry === 'new') {
             let add = prompt('What would you like to add?');
             list.push(add);
+            console.log(`${add} added to list`);
             entry = prompt('What you like to do?');
         }
         if (entry === 'list') {
             for (let print of list) {
-                console.log(`${list.indexOf(print) + 1} ${print}`);
+                console.log(`${list.indexOf(print)} ${print}`);
             }
+            entry = prompt('What you like to do?');
+        }
+        if (entry === 'delete') {
+            let del = prompt('Enter the index of todo to delete:');
+            console.log(`${list[parseInt(del)]} removed from list`);
+            list.splice(del, 1);
             entry = prompt('What you like to do?');
         }
     }
